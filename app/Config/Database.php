fb11de0427efc1122b6bda00990eddb84d2d0d23
roniwahyu <roniwahyu@gmail.com>
2025-06-01 15:04:47 +0700

    update fix

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -27,10 +27,10 @@ class Database extends Config
     public array $default = [
         'DSN'          => '',
         'hostname'     => 'localhost',
-        'username'     => 'postgres',
+        'username'     => 'root',
         'password'     => '',
-        'database'     => 'smartschool',
-        'DBDriver'     => 'Postgre',
+        'database'     => 'simschool1',
+        'DBDriver'     => 'MYSQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
         'DBDebug'      => true,
@@ -41,7 +41,7 @@ class Database extends Config
         'compress'     => false,
         'strictOn'     => false,
         'failover'     => [],
-        'port'         => 5432,
+        'port'         => 3306,
         'numberNative' => false,
         'dateFormat'   => [
             'date'     => 'Y-m-d',
